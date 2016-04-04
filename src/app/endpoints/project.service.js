a07d6d9609ee879644973d7ca8508d60c67ddb24
Marcelo <marcelo.carmona@egoargentina.com>
2016-04-04 04:53:58 -0300

    agrego bower y gulp

--- src/app/endpoints/project.service.js
+++ src/app/endpoints/project.service.js
@@ -11,10 +11,10 @@
     var currentId = 3;
 
     var projectList = [
-    {id:'0',title: 'titulo0',description:'probadooo..', status:'approved', rfi:'123213', notes:'nota ...'},
-    {id:'1',title: 'titulo1',description:'probadooo..', status:'pending', rfi:'123213', notes:'nota ...'},
-    {id:'2',title: 'titulo2',description:'probadooo..', cancelled:'pending', rfi:'123213', notes:'nota ...'},
-    {id:'3',title: 'titulo3',description:'probadooo..'}]
+    {id:'0',title: 'Project 0',description:'description', status:'approved', rfi:'123213', notes:'nota'},
+    {id:'1',title: 'Project 1',description:'description', status:'pending', rfi:'123213', notes:'nota'},
+    {id:'2',title: 'Project 2',description:'description', cancelled:'pending', rfi:'123213', notes:'nota'},
+    {id:'3',title: 'Project 3',description:'description'}]
 
     var service = {
       addProject: addProject,
